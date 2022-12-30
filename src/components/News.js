f315e9d2b3063802bf9bf848ace9ec257a09e982
Tananjay-git <83783632+Tananjay-git@users.noreply.github.com>
2022-12-30 19:46:51 +0530

    news iten edit

--- src/components/News.js
+++ src/components/News.js
@@ -4,12 +4,19 @@ import NewsItem from './NewsItem'
 export class News extends Component {
   render() {
     return (
-      <div>
-        <h2>This is the news component</h2>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewsApp - Top headlines</h2>
+        <div className="row">
+          <div className="col-md-3 mx-3">
+            <NewsItem title="my title" description="my Description"/>
+          </div>
+          <div className="col-md-3 mx-3">
+            <NewsItem title="my title" description="my Description"/>
+          </div>
+          <div className="col-md-3 mx-3">
+            <NewsItem title="my title" description="my Description"/>
+          </div>
+        </div>
       </div>
     )
   }
