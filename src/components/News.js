9d44c80b9e06b348580619b4b72f7397fe4e22b7
Tananjay-git <83783632+Tananjay-git@users.noreply.github.com>
2022-12-31 13:26:44 +0530

    design news item

--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+  
   render() {
     return (
       <div className='container my-3'>
         <h2>NewsApp - Top headlines</h2>
         <div className="row">
           <div className="col-md-3 mx-3">
-            <NewsItem title="my title" description="my Description"/>
+            <NewsItem title="my title" description="my Description" imgUrl="https://images.wsj.net/im-589547/social"/>
           </div>
           <div className="col-md-3 mx-3">
             <NewsItem title="my title" description="my Description"/>
